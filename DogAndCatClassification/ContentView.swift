5d63d63b3826e845a5fa70fb7ee028cab77c7bd5
富澤明大 <akihirowwzai04a@gmail.com>
2024-08-03 16:11:27 +0900

    Initial Commit

---
+++ DogAndCatClassification/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DogAndCatClassification
+//
+//  Created by 富澤明大 on 2024/08/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DogAndCatClassification/DogAndCatClassificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogAndCatClassificationApp.swift
+//  DogAndCatClassification
+//
+//  Created by 富澤明大 on 2024/08/03.
+//
+
+import SwiftUI
+
+@main
+struct DogAndCatClassificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
